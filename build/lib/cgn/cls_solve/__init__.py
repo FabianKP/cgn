a3984c5f4ce0359c0ce2a430750a2fc3a521e559
Fabian Parzer <fabian.kai.parzer@univie.ac.at>
2022-01-28 10:57:13 +0100

    Removed committed but unimportant files such as egg-, build-, and doc-files.

--- build/lib/cgn/cls_solve/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This module contains the basic class and method for setting up and solving (linearly) constrained linear least-squares
-problems.
-"""
-
-from .cls import CLS
-from .cls_solve import cls_solve
--- build/lib/cgn/cnls_solve/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-from .cnls_solve import cnls_solve
-from .cnls import CNLS
-from .cnls_constraint import CNLSConstraint, ConcreteConstraint, NullConstraint
